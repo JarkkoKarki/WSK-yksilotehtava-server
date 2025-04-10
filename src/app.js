544d89b05c8f5a8bef1721b45708a48a18c3ec1b
JarkkoKarki <karkijarkko@gmail.com>
2025-04-10 17:48:46 +0300

    Define __dirname for ES module compatibility

--- src/app.js
+++ src/app.js
@@ -4,7 +4,13 @@ import api from './api/index.js';
 const app = express();
 import cors from 'cors';
 import path from 'path';
+import {fileURLToPath} from 'url';
 import {errorHandler, notFoundHandler} from './middlewares.js';
+
+// Define __dirname for ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors());
 
 app.use('/public', express.static('public'));
